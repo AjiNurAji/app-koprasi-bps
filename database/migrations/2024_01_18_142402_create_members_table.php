b176c98dce94414c0a8291ec1cdd8fd94c3de1d9
AjiNurAji <ajinuraji090306@gmail.com>
2024-01-18 21:29:10 +0700

    starter project, create migration

---
+++ database/migrations/2024_01_18_142402_create_members_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id('id_member');
+            $table->string('email', 60)->unique();
+            $table->string('name', 35);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
