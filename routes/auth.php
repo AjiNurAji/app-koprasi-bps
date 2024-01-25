9f86bbd07f3ee1a36530d19f944aadad6662c880
AjiNurAji <ajinuraji090306@gmail.com>
2024-01-26 04:05:46 +0700

    create login page

---
+++ routes/auth.php
@@ -0,0 +1,14 @@
+<?php
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthController::class, 'index'])->name('login');
+});
+
+Route::middleware('auth')->group(function () {
+    // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+    // Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+    //             ->name('logout');
+});
