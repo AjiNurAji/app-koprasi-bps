64f134a8b409924520b2073ab7285aafccfe1664
AjiNurAji <ajinuraji090306@gmail.com>
2024-02-01 11:50:34 +0700

    fix error admin login

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthController::class, 'index'])->name('login');
     Route::post('login', [AuthController::class, 'processLogin'])->name('login_admin');
 
-    Route::post('login', [AuthController::class, 'processLoginMember'])->name('login_member');
+    Route::post('login-member', [AuthController::class, 'processLoginMember'])->name('login_member');
 });
 
 Route::middleware('auth')->group(function () {
