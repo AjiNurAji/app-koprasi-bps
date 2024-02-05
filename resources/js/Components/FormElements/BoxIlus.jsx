a8c955720bd8e736eb577f7ec98c16859f59c035
AjiNurAji <ajinuraji090306@gmail.com>
2024-02-05 15:56:32 +0700

    upadate login page

--- resources/js/Components/FormElements/BoxIlus.jsx
+++ resources/js/Components/FormElements/BoxIlus.jsx
@@ -1,11 +1,11 @@
 const BoxIlus = () => {
     return (
-        <div className="basis-1/2 h-screen flex justify-center items-center bg-primary dark:bg-boxdark">
-            <div className="flex flex-col w-full justify-center items-center gap-7">
-                <h1 className="text-center text-white font-bold text-3xl">
-                    Welcome to <br /> Koperasi Kesejahteraan.
+        <div className="basis-1/2 hidden h-screen md:flex justify-center items-center bg-primary dark:bg-boxdark">
+            <div className="flex flex-col w-full justify-center items-center gap-20">
+                <h1 className="text-center text-white font-extrabold text-title-xl">
+                    Welcome to <br /> Koperasi Kesejahteraan
                 </h1>
-                <div className="w-2/3 overflow-hidden h-auto">
+                <div className="w-[45%] overflow-hidden h-auto">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         data-name="Layer 1"
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -45,87 +45,10 @@ const Login = () => {
     return (
         <>
             <Head title="Log in" />
-            {/* <div className="flex justify-center items-center min-h-screen bg-bodydark1 dark:bg-black">
-                <div className="rounded-md border w-full sm:w-fit border-stroke bg-white shadow-default p-5 dark:border-strokedark dark:bg-boxdark">
-                    <div className="flex justify-center items-center flex-col gap-3">
-                        <div className="flex justify-center items-center flex-col gap-2">
-                            <div className="w-20 h-auto overflow-hidden">
-                                <img className="w-full" src={Logo} alt="Logo" />
-                            </div>
-                            <h1 className="text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                                Log in
-                            </h1>
-                        </div>
-                        <form
-                            ref={form}
-                            onSubmit={submit}
-                            method="post"
-                            className="flex flex-col justify-start gap-5 items-start"
-                        >
-                            <div>
-                                <label
-                                    htmlFor="username"
-                                    className="mb-2.5 font-medium text-black dark:text-white"
-                                >
-                                    Username
-                                </label>
-                                <input
-                                    type="text"
-                                    autoComplete="username"
-                                    id="username"
-                                    name="username"
-                                    ref={inputUsername}
-                                    required
-                                    onChange={(e) => handleValue(e)}
-                                    placeholder="Masukkan username anda"
-                                    className="w-full rounded-md border text-dark dark:text-white border-stroke bg-transparent py-2 pl-4 pr-6 transition-all duration-300 ease-in-out outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                />
-                            </div>
-                            <div className="w-full">
-                                <label
-                                    htmlFor="password"
-                                    className="mb-2.5 font-medium text-black dark:text-white"
-                                >
-                                    Password
-                                </label>
-                                <div className="relative w-full">
-                                    <input
-                                        type={hide ? "password" : "text"}
-                                        name="password"
-                                        required
-                                        onChange={(e) => handleValue(e)}
-                                        autoComplete="current-password"
-                                        id="password"
-                                        placeholder="Masukkan password anda"
-                                        className="relative w-full rounded-md border text-dark dark:text-white border-stroke bg-transparent py-2 pl-4 pr-6 transition-all duration-300 ease-in-out outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                                    />
-                                    <div onClick={() => setHide(!hide)} className="absolute top-3.5 right-4 cursor-pointer text-body hover:text-black dark:hover:text-white">{hide ? <PiEyeLight /> : <PiEyeSlash />}</div>
-                                </div>
-                            </div>
-                            <div>
-                                <Checkbox data={data} setData={setData} />
-                            </div>
-                            <div className="w-full">
-                                {processing ? (
-                                    <ButtonLoading color="primary" />
-                                ) : (
-                                    <button
-                                        type="submit"
-                                        name="button-sumbit"
-                                        className="w-full cursor-pointer rounded-md border border-primary bg-primary p-2 text-white transition hover:bg-opacity-90"
-                                    >
-                                        Log in
-                                    </button>
-                                )}
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> */}
             <div className="flex items-center justify-between min-h-screen w-full">
                 <BoxIlus />
-                <div className="basis-1/2 h-screen flex justify-center items-center bg-white dark:bg-boxdark-2">
-                    <div className="flex justify-center items-center flex-col gap-3 w-9/12">
+                <div className="md:basis-1/2 basis-full h-screen flex justify-center items-center bg-white dark:bg-boxdark-2">
+                    <div className="flex justify-center items-center flex-col gap-3 w-full">
                         <div className="flex justify-center items-center flex-col gap-2">
                             <div className="w-20 h-auto overflow-hidden">
                                 <img className="w-full" src={Logo} alt="Logo" />
